ae31e1e78f1dd7137441944cd0c424e5b9c891d5
Ari Mahpour <Ari.Mahpour@gmail.com>
2025-08-31 15:22:56 -0700

    Cleaned up DEBUG 2.

--- shared/core/node.c
+++ shared/core/node.c
@@ -128,7 +128,7 @@ void node_begin(Node* n) {
     Frame in;
     int heard_claim = 0;
     uint32_t listen_start = hal_millis();
-    uint32_t listen_end = listen_start + 200;
+    uint32_t listen_end = listen_start + 1000;
     uint32_t last_debug = 0;
 
     while (hal_millis() < listen_end) {
